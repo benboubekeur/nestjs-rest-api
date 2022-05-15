ed26dc1740c2d4de47e5397479960d5e68a530a8
benb <boumedyen121@gmail.com>
2022-05-15 18:02:19 +0100

    FEAT / Login using tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
-
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {
   }
 
-
-  @Post("signin")
-  signing(@Body() dto: AuthDto) {
-    return this.authService.login(dto);
-  }
-
   @Post("signup")
   signup(@Body() dto: AuthDto) {
-    console.log(dto.email, dto.password);
     return this.authService.signup(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post("signin")
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,12 @@
-import {Module} from "@nestjs/common";
-import {AuthController} from "./auth.controller";
-import {AuthService} from "./auth.service";
-import {PrismaModule} from "../prisma/prisma.module";
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [AuthController],
-    providers: [AuthService]
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule {
 }
