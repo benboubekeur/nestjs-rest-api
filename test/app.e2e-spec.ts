e0c6ae297e7eae2be8db2bf9a440202581360c93
benb <boumedyen121@gmail.com>
2022-05-27 18:23:06 +0100

    FEAT / Add env testing file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,32 @@
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../src/app.module";
+
+import { PrismaService } from "../src/prisma/prisma.service";
+
+
 describe("App e2e", () => {
-  it.todo("Hello");
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule]
+      }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true
+      })
+    );
+    await app.init();
+
+  });
+  it.todo("Should pass");
+  afterAll(() => {
+    app.close();
+  });
+
 });
